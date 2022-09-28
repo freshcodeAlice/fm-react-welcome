ce4714bb668f4d920f0fe2d1a8021b939671713a
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-28 10:26:53 +0300

    Class Component

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+class HeaderComponent extends React.Component {
+
+    render() {
+        const p = React.createElement('p', {}, 'Super paragraph');
+        const h2 = React.createElement('h2', {title: 'Hi!'}, 'Header!');
+        const article = React.createElement('article', {}, h2, p);
+        return article
+    }
+}
+
+const component = React.createElement(HeaderComponent);
+
 const root = document.querySelector('#root');
-const h1 = React.createElement('h1', {title: 'Hello'}, 'Hello, REact!');
-console.log(h1);
-const anotherH1 = document.createElement('h1');
-console.dir(anotherH1);
-
-console.log(ReactDOM);
-
-ReactDOM.render(h1, root);
+ReactDOM.render(component, root);
